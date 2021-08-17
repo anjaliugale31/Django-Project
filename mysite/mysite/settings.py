0df09d02da5c706a1bd2986351c826b9d5330507
anjali <anjali.ugale@agnie.com>
2021-08-17 10:58:10 +0530

    Change

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-_q$&mng62=u9-)$dn^52en%icdtajc)yfaos1&in7g2)3qz0k!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 # Application definition
 
@@ -78,7 +77,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -107,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 

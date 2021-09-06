d036b506fd484a918a8e28dee28d6daa8fc2ea2f
anjali <anjaliugale52@gmail.com>
2021-09-06 12:57:36 +0530

    changes..

--- mysite/blog/management/commands/scrap.py
+++ mysite/blog/management/commands/scrap.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 from django.core.management.base import BaseCommand
 import requests
-from pyquery import PyQuery 
+from pyquery import PyQuery
 from django.utils import timezone
 
 
@@ -20,40 +20,31 @@ class Command(BaseCommand):
         request1=requests.get('https://www.tutorialspoint.com/python3/index.htm')
         html=PyQuery(request1.text)   #created pyquery object
         pages=list(html(".chapters a").items())  #scraped all html Anchor tag
-        total_pages=len(pages)
+        total_pages=len(pages) #total pages on site
         all_data=[]
         for tag in range(total_pages):
             if(count<page_no):
                 each_page=requests.get('https://www.tutorialspoint.com'+pages[tag].attr('href'))
                 each_html=PyQuery(each_page.text)
                 title_html=each_html(".tutorial-content h1").text()
-                text_each=each_html(".tutorial-content")
-                text_each("h1").remove()
-                text_each("#google-top-ads").remove()
-                text_each('.google-bottom-ads').remove()
-                text_each('#bottom_navigation').remove()
-                text_each('.tutorial-menu').remove()
-                text_each('.button-borders').remove()
-                text_val=text_each.text()
-                
-                text_img=each_html(".cover img").attr("src")
-
-                if(text_img):
-                    img_name=each_html(".cover img").attr("src").split("/")[-1]
-                    img_url="https://www.tutorialspoint.com"+text_img
-
-                    # one way by downloading image
-                    # image_filename = wget.download(img_url,"media/documents")
-                    # print(image_filename)
-
-                    # 2nd way by creating new directory and displaying the image 
-                    with open('media/documents/'+img_name,'wb') as img_file:
-                        image=requests.get(img_url)
-                        img_file.write(image.content)
-                if(Post.objects.filter(title=title_html)):
-                    # print("Already in database")
+                if(Post.objects.filter(title=title_html)).exists():
                     continue
                 else:
+                    text_each=each_html(".tutorial-content")
+                    text_each("h1").remove()
+                    text_each("#google-top-ads").remove()
+                    text_each('.google-bottom-ads').remove()
+                    text_each('#bottom_navigation').remove()
+                    text_each('.tutorial-menu').remove()
+                    text_each('.button-borders').remove()
+                    text_val=text_each.text()
+                    text_img=each_html(".cover img").attr("src")
+                    if(text_img):
+                        img_name=each_html(".cover img").attr("src").split("/")[-1] #getting name of img 
+                        img_url="https://www.tutorialspoint.com"+text_img
+                        with open('media/documents/'+img_name,'wb') as img_file:# 2nd way by creating new directory and displaying the image 
+                            image=requests.get(img_url)
+                            img_file.write(image.content)
                     scraped_data=Post(author=User.objects.get_or_create(username="demo")[0],
                     title=title_html,
                     text=text_val,
@@ -62,7 +53,7 @@ class Command(BaseCommand):
                     all_data.append(scraped_data)
                     count+=1
         Post.objects.bulk_create(all_data)
-        #condition for 
+        #checking for total pages where scrap 
         if(count>0):
             print("Number of pages scrap :",count)
         else:
